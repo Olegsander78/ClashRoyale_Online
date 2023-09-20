1fc28d2a347d63b849122b29ffab5cec9f510c1b
Oleg K <onkotov@gmail.com>
2023-09-20 16:42:20 +0500

    Добавил сцену авторизации и скрипт Network

---
+++ Assets/Gameplay/Scripts/Network/Network.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ClashRoyale
+{
+    public class Network : MonoBehaviour
+    {
+        public void Post(string uri, Dictionary<string, string> data, Action<string> success, Action<string> error = null)
+            => StartCoroutine(PostRoutine(uri, data, success, error));
+
+        private IEnumerator PostRoutine(string uri, Dictionary<string, string> data, Action<string> success, Action<string> error = null)
+        {
+            using(UnityWebRequest www = UnityWebRequest.Post(uri, data))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                    error?.Invoke(www.error);
+                else
+                    success?.Invoke(www.downloadHandler.text);
+            }
+        }
+    }
+}
